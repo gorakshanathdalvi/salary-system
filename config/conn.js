c0b8120dc9b34f78acccc9bbf071f504102098d2
Gorakshnath Dalvi <gorakshnathdalavi91@gmail.com>
2026-05-17 09:40:25 +0530

    Update database connection configuration;

--- config/conn.js
+++ config/conn.js
@@ -3,10 +3,10 @@ var mysql = require('mysql');
 var util = require('util');
 
 var conn = mysql.createConnection({
-  host: 'localhost', 
-  user:'root',
-    password:'',
-    database:'sandip_software'
+  host: 'bkudzgyjtxpv4p9ti1c4-mysql.services.clever-cloud.com', 
+  user:'u35mczdxqvxcyoti',
+    password:'I6L648MB57MPvN8rjWQx',
+    database:'bkudzgyjtxpv4p9ti1c4'
 });
 
 var exe = util.promisify(conn.query).bind(conn);
